fdf7281a34949befe91af76d4779c97d980f3d07
Biraj Kumar Biswas <toshebamail@gmail.com>
2021-10-30 17:31:39 +0600

    Database link Remove

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const MONGDB_URI = 'mongodb+srv://SM:studentmanagment@newproject.92n8k.mongodb.net/students'
 app.set('views engin','ejs')
 
 app.get('/concat',(req,res,next)=>{
@@ -21,13 +20,5 @@ app.get('*',(re,res,next)=>{
 	})
 })
 const PORT = process.env.PORT || 8000
-mongoose.connect(MONGDB_URI,
-		{useNewUrlParser:true},
-		()=>{
-			app.listen(PORT,()=>{
-				console.log(`Server Is Running PORT ${PORT}`)
-				console.log('Database Is Running Now')
-			})
-		}
-
-) 
+app.listen(PORT,()=>console.log(`Server Is Running On PORT ${PORT}`))
+				
