6264c9c11afb61058136962c72608dee37a2e643
Biraj Kumar Biswas <toshebamail@gmail.com>
2021-10-30 15:44:58 +0600

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const MONGDB_URI = 'mongodb+srv://SM:studentmanagment@newproject.92n8k.mongodb.net/students'
+
 app.set('views engin','ejs')
 
 app.get('/',(req,res,next)=>{
@@ -13,13 +13,4 @@ app.get('*',(re,res,next)=>{
 	})
 })
 const PORT = process.env.PORT || 8000
-mongoose.connect(MONGDB_URI,
-		{useNewUrlParser:true},
-		()=>{
-			app.listen(PORT,()=>{
-				console.log(`Server Is Running PORT ${PORT}`)
-				console.log('Database Is Running Now')
-			})
-		}
-
-) 
+app.listen(PORT,()=>console.log(`Server Is Ready To Now PORT ${PORT}`))
