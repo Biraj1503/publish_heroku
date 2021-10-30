0862de74e3bc5bbe13f7e4568adb67dcbcc2bc37
Biraj Kumar Biswas <toshebamail@gmail.com>
2021-10-30 15:08:04 +0600

    add some code index.js

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ app.set('views engin','ejs')
 app.get('/',(req,res,next)=>{
 	res.render('home.ejs')
 })
+
+app.get('*',(re,res,next)=>{
+	res.json({
+		message:"404 Not Found"
+	})
+})
 const PORT = process.env.PORT || 8000
 app.listen(PORT,()=>console.log(`Server Is Ready To Now PORT ${PORT}`))
