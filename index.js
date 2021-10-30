683ae90c72ef41380127133da29d06e3dd83b1db
Biraj Kumar Biswas <toshebamail@gmail.com>
2021-10-30 15:33:29 +0600

    change two route

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const MONGDB_URI = 'mongodb+srv://SM:studentmanagment@newproject.92n8k.mongodb.net/students'
 app.set('views engin','ejs')
 
-/*app.get('/',(req,res,next)=>{
+app.get('/',(req,res,next)=>{
 	res.render('home.ejs')
-})*/
+})
 
 /*app.get('/concat',(req,res,next)=>{
 	res.json({
@@ -13,14 +13,14 @@ app.set('views engin','ejs')
 		Home:"Paterhat,Mehendigonj"
 	})
 })
-*/
+
 app.get('/',(req,res,next)=>{
 	res.json({
 		name:"Biraj Kumar Biswas",
 		email:"toshebamail@gmail.com",
 		age:27
 	})
-})
+})*/
 
 app.get('*',(re,res,next)=>{
 	res.json({
